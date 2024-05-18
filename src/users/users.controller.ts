f66cdfdfb83e4c47564231e1a9137342bb3828b6
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2024-05-18 20:58:45 +0500

    add basic routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() //GET /users
+  findAll() {
+    return [];
+  }
+
+  @Get(':id') //GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() //POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') //PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+}
