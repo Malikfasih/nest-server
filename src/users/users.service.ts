55c7d8caa389f10efca5699cdda204467cd636b9
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2024-05-20 17:30:15 +0500

    add methods and complete our service class

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -41,18 +41,51 @@ export class UsersService {
     }
   }
 
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
+  }
+
   create(user: {
     name: string;
     email: string;
     role: 'INTERN' | 'ENGINEER' | 'ADMIN';
   }) {
     // generate Id
-    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const usersByHighestId = this.users.sort((a, b) => b.id - a.id);
+    // console.log('usersByHighestId ->', usersByHighestId);
     const newUser = {
       id: usersByHighestId[0].id + 1,
       ...user,
     };
     this.users.push(newUser);
     return newUser;
+  }
+
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+
+    this.users = this.users.filter((user) => user.id !== id);
+
+    return removedUser;
   }
 }
